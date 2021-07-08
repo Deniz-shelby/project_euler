76572aadd84fdd87c8317754413237e653d66da6
Deniz-shelby <d.elci@hotmail.de>
2021-07-08 16:45:20 +0200

    Update bubble_sort.py

--- sorting_algo/bubble_sort.py
+++ sorting_algo/bubble_sort.py
@@ -1,9 +1,10 @@
 def bubble_sort(a_list):
     n = len(a_list)
-    for i in range(n-1):
+    for i in range(n):
         for j in range(0,n-i-1):
-            if a_list[j] > a_list[j +1]:
+            if a_list[j+1] < a_list[j]:
                 a_list[j], a_list[j + 1] =a_list[j + 1],a_list[j]
+    return a_list
 
 test_list = [64, 34, 25, 12, 22, 11, 90]
 

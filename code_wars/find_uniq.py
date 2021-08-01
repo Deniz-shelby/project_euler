87a7af7b9c381de77445b77cf5b92d81242b1a15
Deniz-shelby <d.elci@hotmail.de>
2021-08-01 18:42:34 +0200

    Create find_uniq.py

---
+++ code_wars/find_uniq.py
@@ -0,0 +1,17 @@
+arr = ([1,2,3,1,2,1])
+
+# to slow
+#
+# def find_uniq(arr):
+#     for i in arr:
+#         if arr.count(i) == 1:
+#             return i
+
+
+def find_uniq(arr):
+    all_numbers = set(arr)
+    return all_numbers
+
+
+print(find_uniq(arr))
+        
